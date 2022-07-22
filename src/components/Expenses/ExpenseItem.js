0e412da27941d51a626c9bd863ee9905a1ac27da
Iqbal Ajie Wahyudin <iqbalajiewahyudin@gmail.com>
2022-07-22 18:34:51 +0700

    create chart

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,18 @@
-import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "../Expenses/ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  console.log("Re-create by React");
-
-  const clickHandler = () => {
-    console.log(title);
-    setTitle("Title Changed");
-  };
-
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Button</button>
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -46,7 +46,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -88,7 +88,10 @@ const ExpenseForm = (props) => {
           />
         </div>
       </div>
-      <div className='new-expense__action'>
+      <div className='new-expense__actions'>
+        <button type='button' onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
